04291854308c36796d984de1a4867b3fe3db129c
Ashwanth K <97865565+AK16092003@users.noreply.github.com>
2022-08-29 21:11:00 +0530

    Add files via upload

--- queue.c
+++ queue.c
@@ -1,54 +1,118 @@
 #include<stdio.h>
 #include<conio.h>
 
+int size = 100;
+int queue[100];
+int front = 0;
+int rear  = 0;
+
+void enqueue(int x)
+{
+	if(front == rear && queue[front]!=0)
+	{
+		printf("Overflow\n");
+		return ;
+	}
+	queue[rear] = x;
+	rear = (rear + 1)%size;
+}
+
+int dequeue()
+{
+	if(front == rear && queue[front]==0)
+	{
+		printf("Underflow\n");
+		return 0;
+	}
+	int del = queue[front];
+	queue[front] = 0;
+	front = (front + 1)%size;
+	return del;
+}
+
+void display()
+{
+	if(front < rear)
+	{
+		for(int i = front ; i < rear ; i ++)
+		{
+			printf("%d ",queue[i]);
+		}
+	}
+	else
+	{
+		for(int i = front ; i < size ; i ++)
+		{
+			printf("%d ",queue[i]);
+		}
+		for(int i = 0 ; i < rear ; i ++)
+		{
+			printf("%d ",queue[i]);
+		}
+	}
+}
+
+int isempty()
+{
+	if(front == rear && queue[rear] == 0)
+	{
+		return 1;
+	}
+	return 0;
+}
+
 int main()
 {
-	int n;
-	printf("Enter the number n : ");
-	scanf("%d", &n);
-	int queue[n];
-	int front , rear;
-	front = 0;
-	rear = -1;
-	printf("Operations \n");
-	printf("1. enqueue\n2. dequeue\n3. print queue\n");
+	
+	printf("1) Enqueue a new element\n");
+	printf("2) Dequeue a element\n");
+	printf("3) Print all elements in queue\n");
+	printf("4) Front and back values\n");
+	printf("5) Check whether queue is empty\n");
 	
 	while(1)
 	{
-		
+		int ch;
 		printf("Enter your choice : ");
-		int choice = 0;
-		scanf("%d" , &choice);
+		scanf("%d" , &ch);
 		
-		if(choice == 1)
+		if(ch == 1)
 		{
+			printf("Enter a element : ");
 			int x;
-			printf("Enter element : ");
 			scanf("%d" , &x);
-			rear++;
-			rear = rear%n;
-			queue[rear] = x;
+			enqueue(x);	
 		}
-		else if(choice == 2){
-			front++;
-			front = front%n;
+		else if(ch == 2)
+		{
+			int element = dequeue();
+			printf("Deleted element : %d" , element);
 		}
-		else{
-			if(rear < front)
+		else if(ch == 3)
+		{
+			display();
+		}
+		else if(ch == 4)
+		{
+			printf("front : %d\nback : %d" , queue[front] , queue[(rear - 1)<0?(rear-1+size):(rear-1)]);	
+		}	
+		else if(ch == 5)
+		{
+			if(isempty())
 			{
-				for(int i = front ; i <= rear+n ; i ++)
-				{
-					printf("%d ",queue[i%n]);
-				}
+				printf("queue is empty");
 			}
-			for(int i = front ; i <= rear ; i ++ )
+			else
 			{
-				printf("%d ",queue[i%n]);
+				printf("queue is not empty");
 			}
 		}
-		
+		else
+		{
+			printf("Invalid input");
+		}
 		printf("\n");
-		printf("front = %d rear = %d \n",front,rear);
 	}
+	
 	return 0;
 }
