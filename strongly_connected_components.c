6fb19a4b8079e7a5f487ff9023d7a38dd4d4745e
Ashwanth K <97865565+AK16092003@users.noreply.github.com>
2022-10-17 00:44:13 +0530

    Add files via upload

---
+++ strongly_connected_components.c
@@ -0,0 +1,80 @@
+// DFS
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct adjlstnode
+{
+	int data;
+	struct adjlstnode *next;
+};
+
+struct adjlstnode * insert(int data , struct adjlstnode *head)
+{
+	struct adjlstnode *new_node = (struct adjlstnode *)malloc(sizeof(struct adjlstnode));
+	new_node->data = data;
+	new_node->next = head;
+	head = new_node;
+	return head;
+}
+
+int visited[1000];
+struct 	adjlstnode *adj_lst[1000];
+
+void dfs(int u)
+{
+	visited[u] = 1;
+	printf("%d " , u+1);
+	struct adjlstnode *cur = adj_lst[u];
+	while(cur!=NULL)
+	{
+		if(visited[cur->data] == 0)
+			dfs(cur->data);
+		cur = cur->next;
+	}
+}
+
+int main()
+{
+	printf("Enter number of nodes N = ");
+	int n;
+	scanf("%d" , &n);
+	for(int i = 0 ; i < n ; i ++) visited[i] = 0;
+	 	
+	printf("Enter the matrix: \n");
+	int adjmat[n][n];
+	
+
+	for(int i = 0 ; i < n ; i ++)
+	{
+		adj_lst[i] = NULL;
+		
+		for(int j = 0 ; j < n ; j ++)
+		{
+			scanf("%d" , &adjmat[i][j]);
+			if(adjmat[i][j] == 1)
+			{
+				// insert a new node
+				adj_lst[i] = insert(j , adj_lst[i]);
+			}
+		}
+		
+	}
+	printf("\n");
+	int c = 0;
+	for(int i = 0 ; i < n ; i ++)
+	{
+		//printf("vertex %d :" , i+1);
+		if(visited[i] == 0)
+		{
+			printf("Component %d : " , c+1);
+			dfs(i);
+			printf("\n");
+			c++;
+		}
+	}
+	printf("Total components : %d\n" , c);
+	
+	return 0;
+}
